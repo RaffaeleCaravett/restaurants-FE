3d7a9c95288bf66a21f9150f25f727ee70107429
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-03-01 10:08:14 +0100

    add prodotto done

--- src/app/components/office/office.component.ts
+++ src/app/components/office/office.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { OfficeService } from 'src/app/services/office.service';
 
 @Component({
@@ -15,7 +16,8 @@ tipoProdotto:string[]=['ANTIPASTO','PRIMO','SECONDO','PIZZA_BIANCA','PIZZA_ROSSA
 menu:any
 ingredienti:any[]=[]
 chosedIngredients:any[]=[]
-constructor(private officeService:OfficeService){}
+
+constructor(private officeService:OfficeService,private toastr:ToastrService){}
 
 ngOnInit(): void {
 if(localStorage.getItem('restaurant')){
@@ -34,22 +36,74 @@ this.officeService.getAllIngredienti().subscribe((ingredienti:any)=>{
     this.ingredienti=ingredienti
   }
 })
+this.getAllProdotti()
 }
 
 goToOffice(){
   this.office=!this.office
 }
 updateIngredienti(ingrediente:any){
-  console.log(ingrediente)
+let ingr;
 let isThereIngredient:boolean=false
-  this.chosedIngredients.forEach((i:any)=>{
-if(i.id==ingrediente.id){
+this.ingredienti.forEach((ing:any)=>{
+  if(ing.id==ingrediente){
+    ingr=ing
+    this.chosedIngredients.forEach((i:any)=>{
+if(i.id==ing.id){
   isThereIngredient=true;
 }
 })
+  }
+})
+
 
 if(!isThereIngredient){
-  this.chosedIngredients.push(ingrediente)
+  this.chosedIngredients.push(ingr)
 }
+}
+removeItemFromChosed(item:any){
+let newIngredients:any[]=[]
+  this.chosedIngredients.forEach((ingredients:any)=>{
+    if(ingredients.id!=item.id){
+      newIngredients.push(ingredients)
+    }
+  })
+
+
+  this.chosedIngredients=newIngredients
+}
+addProdotto(){
+if(this.prodottoForm.valid){
+  let ingredients:any[]=[]
+  this.chosedIngredients.forEach((i:any)=>{
+    ingredients.push(i.id)
+  })
+
+  this.officeService.addProdotti({
+    nome:this.prodottoForm.controls['nome'].value,
+    prezzo:this.prodottoForm.controls['prezzo'].value,
+    tipoProdotto:this.prodottoForm.controls['tipoProdotto'].value,
+    esercizio_id:this.esercizio.id,
+    ingredienti_id:ingredients
+  }).subscribe({
+    next:(prodotto:any)=>{
+      if(prodotto){
+        this.getAllProdotti()
+      }
+    },
+    error:(err:any)=>{
+      this.toastr.show(err.error.message||"Qualcosa è andato storto nel salvataggio del prodotto.")
+    },
+    complete:()=>{}
+  })
+}
+}
+
+getAllProdotti(){
+this.officeService.getAllProdotti().subscribe((prodotto:any)=>{
+if(prodotto){
+  this.menu=prodotto
+}
+})
 }
 }
--- src/app/services/office.service.ts
+++ src/app/services/office.service.ts
@@ -13,10 +13,18 @@ export class OfficeService{
 
 
 public ingrediente:string='/ingrediente'
+public prodotto:string='/prodotto'
+
 
 constructor(private http:HttpClient){}
 
 getAllIngredienti(){
   return this.http.get(environment.API_URL+this.ingrediente)
+}
+getAllProdotti(){
+  return this.http.get(environment.API_URL+this.prodotto)
+}
+addProdotti(prodotto:{}){
+  return this.http.post(environment.API_URL+this.prodotto,prodotto)
 }
 }
